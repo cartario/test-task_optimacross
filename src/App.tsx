eed39e50a4c40f476d2fd2da7a56aa1ebf70ca1b
VasiliyMac <cartario@yandex.ru>
2022-06-12 12:47:00 +0300

    верстка

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,104 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import OldApp from './components/old/oldApp';
 import './App.css';
+import './Optimacross.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+      <header className="App-header">        
+        {/* <OldApp/>/ */}
+        <div className="board">
+          <div className="data">
+
+            <div className="data__col list">
+
+              <p className="list__item">Root</p>
+              <ul>
+                <li>
+                  <p className="list__item list__item--level_1">Level-1</p>
+                  <ul>
+                    <li className="list__item list__item--level_2">Level-2</li>
+                  </ul>
+                </li>
+              </ul>
+
+              <p className="list__item">Root</p>
+              <ul>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>   
+                  <ul>
+                    <li className="list__item list__item--level_2">Level-2</li>
+                    <li className="list__item list__item--level_2">Level-2</li>
+                    <li className="list__item list__item--level_2">Level-2</li>
+                    <li className="list__item list__item--level_2">Level-2</li>
+                  </ul>               
+                </li>
+              </ul>
+
+              <p className="list__item">Root</p>
+              <ul>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+              </ul>
+
+              <p className="list__item">Root</p>
+              <ul>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+              </ul>
+
+              <p className="list__item">Root</p>
+              <ul>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+              </ul>
+
+              <p className="list__item">Root</p>
+              <ul>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+                <li className="list__label">
+                  <p className="list__item list__item--level_1">Level-1</p>                  
+                </li>
+              </ul>
+
+
+            </div>
+
+            <div className="data__col info">
+              <ul className="info__list">
+                <li className="info__item">Label: X</li>
+                <li className="info__item">Id: X</li>
+                <li className="info__item">ParentId: X</li>
+              </ul>              
+            </div>
+            
+          </div>          
+
+          <div className="controls">
+            <button className="controls__btn controls__btn--apply">Apply</button>
+            <button className="controls__btn controls__btn--refresh">Refresh</button>
+            <button className="controls__btn controls__btn--remove">Remove</button>
+          </div>
+
+        </div>
       </header>
     </div>
   );
